5c905099eef445138d27559e32a80fedee286e27
sesia <zoenea@naver.com>
2020-11-11 14:22:34 +0900

    개발 완료 및 엑셀변환 테스트 완료.

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
 import partsArray from "./libs/partsArray";
 import startApp from "./libs/riggingData";
-import craneData from "./libs/craneData";
 
 const workValue = { // input value  
   safetyFactor : 85,
@@ -18,11 +17,11 @@ const workValue = { // input value
   //   vertical2: 0,
   //   height2: 0
   // },
-  workWeight : 1,
+  workWeight : 20,
   workBuilding : {  // 크레인이 건물에 붙는 면을 가로.
-    vertical : 1,
+    vertical : 8,
     horizontal : 0,
-    height : 1,
+    height : 34,
   },
   block : {
     vertical1: 0,
@@ -35,6 +34,15 @@ const workValue = { // input value
 
 // startApp(workValue);
 // console.log(startApp(workValue));
+// partsArray(craneData);
+console.log(partsArray(craneData));
 
-partsArray(craneData);
-// console.log(partsArray(craneData));
+// const craneDataCal = startApp(workValue);
+// const partsInfoData = partsArray(craneDataCal[craneDataCal.length-1]);
+// console.log(craneDataCal[craneDataCal.length-1]);
+// console.log('-------------------------------------------------------------------------------');
+// console.log(partsInfoData.partsList);
+// console.log('-------------------------------------------------------------------------------');
+// console.log(partsInfoData.partsData);
+// console.log('-------------------------------------------------------------------------------');
+// console.log(partsInfoData.connectionData);
